069b605c8a9737a99ab57c5fe7a1aaeda63202fc
Daniel Miller <danielmiller5791@gmail.com>
2018-05-21 12:50:21 +0100

    deposits in bank account now completed. module for processing transaction also included in script

--- src/account.js
+++ src/account.js
@@ -1,8 +1,14 @@
 (function(exports) {
+
+  let transaction = require('./transaction.js');
   
   let bankAccount = function() {
     this.totalMoney = 0;
     this.history = [];
+  };
+
+  bankAccount.prototype.deposit = function(money) {
+    this.totalMoney+= money;
   };
 
   exports.bankAccount = bankAccount;
